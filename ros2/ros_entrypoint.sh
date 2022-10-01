5ce1cd267be7ece566b0e8ee594aaf8e95af48f6
Oshadha Gunasekara <oshadha123@gmail.com>
2022-09-30 21:16:39 -0500

    fix entrypoint

--- ros2/ros_entrypoint.sh
+++ ros2/ros_entrypoint.sh
@@ -2,11 +2,9 @@
 set -e
 
 # setup ros environment
-
-if [ ${ROS_DISTRO} == "humble" ] ; then
+if [ ${ROS_DISTRO} == "foxy" ] ; then
 	source "$ROS2_WS/install/local_setup.bash"
 else
 	source "/opt/ros/$ROS_DISTRO/setup.bash" 
-	source "$ROS_WS/devel/setup.bash"
 fi
 exec "$@"
