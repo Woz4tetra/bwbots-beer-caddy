f3f3403146e815eb15416bc75fa04ad0b261b899
Woz4tetra <woz4tetra@gmail.com>
2022-10-06 10:30:32 -0400

    ros2 docker is working. Need to add other packages

--- ros2/packages/ros_entrypoint.sh
+++ ros2/packages/ros_entrypoint.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -e
 
-ros_env_setup="/opt/ros/$ROS_DISTRO/setup.bash"
+ros_env_setup="/opt/ros/$ROS_DISTRO/install/setup.bash"
 echo "sourcing   $ros_env_setup"
 source "$ros_env_setup"
-source ${HOME}/ros_ws/devel/setup.bash
+
+source ${HOME}/ros_ws/install/local_setup.bash
+source ${HOME}/ros_ws/install/setup.bash
+source /usr/share/colcon_cd/function/colcon_cd.sh
+export _colcon_cd_root=/opt/ros/$ROS_DISTRO
 
 echo "ROS_ROOT   $ROS_ROOT"
 echo "ROS_DISTRO $ROS_DISTRO"
