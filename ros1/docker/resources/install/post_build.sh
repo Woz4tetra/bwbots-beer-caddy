8f8a729b48e0dd0f755b9d71a14c8b45a199f3b2
Woz4tetra <woz4tetra@gmail.com>
2023-01-03 16:51:10 -0500

    move clone script for native build

--- ros1/docker/resources/install/post_build.sh
+++ ros1/docker/resources/install/post_build.sh
@@ -2,10 +2,10 @@
 echo "ros workspace src: ${ROS_WS_SRC}"
 
 cd ${ROS_WS_SRC}
-/root/install/clone_ros_packages.sh
+/root/scripts/clone_ros_packages.sh
 
 if [ ! -f ${ROS_WS_SRC}/PATCH_APPLIED ]; then
-    /root/install/apply_patches.sh ${ROS_WS_SRC}
+    /root/scripts/apply_patches.sh ${ROS_WS_SRC}
     touch ${ROS_WS_SRC}/PATCH_APPLIED
 fi
 
