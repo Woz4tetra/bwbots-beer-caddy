fe09c6b804ece7f8efdc375817f8782b80c2cc0e
Woz4tetra <woz4tetra@gmail.com>
2023-01-09 20:44:06 -0500

    move workstation docker to unity

--- ros1/bwbots/bw_tags/scripts/generate_tag.py
+++ ros1/bwbots/bw_tags/scripts/generate_tag.py
@@ -14,49 +14,69 @@ image_formats = {
     "tagStandard52h13": "tag52_13_%05d.png",
 }
 
-parser = argparse.ArgumentParser(description="tag")
 
-parser.add_argument("tag",
-                    choices=list(image_formats.keys()),
-                    help="tag family")
-parser.add_argument("code",
-                    type=int,
-                    help="tag embedded code")
-parser.add_argument("-p", "--pixels_per_mm",
-                    type=float,
-                    default=10.0,
-                    help="pixels per mm")
-parser.add_argument("-l", "--length_mm",
-                    type=float,
-                    default=200.0,
-                    help="length mm")
+def generate_tag_pdf(tag_family: str, code: int, px_per_mm: float, length_mm: float):
+    file_name = image_formats[tag_family] % code
 
-args = parser.parse_args()
+    url = f"https://github.com/AprilRobotics/apriltag-imgs/raw/master/{tag_family}/{file_name}"
 
-tag_family = args.tag
-code = args.code
+    img_data = requests.get(url).content
+    with open(file_name, "wb") as handler:
+        handler.write(img_data)
+    print("Downloaded image to", file_name)
 
-px_per_mm = args.pixels_per_mm
-length_mm = args.length_mm
+    new_border_size_px = int(length_mm * px_per_mm)
+    print(f"Resizing to {new_border_size_px} px")
 
-file_name = image_formats[tag_family] % code
+    pdf_path = os.path.splitext(file_name)[0] + ".pdf"
 
-url = f"https://github.com/AprilRobotics/apriltag-imgs/raw/master/{tag_family}/{file_name}"
+    dpi = px_per_mm * 25.4
 
-img_data = requests.get(url).content
-with open(file_name, 'wb') as handler:
-    handler.write(img_data)
-print("Downloaded image to", file_name)
+    image = Image.open(file_name)
 
-new_size_px = int(length_mm * px_per_mm)
-print(f"Resizing to {new_size_px} px")
+    assert image.size[0] == image.size[1]
+    tag_border_px_size = image.size[0]
+    tag_px_size = tag_border_px_size - 2
+    assert tag_px_size > 0
+    resize_ratio = new_border_size_px / tag_border_px_size
+    new_tag_px_size = int(tag_px_size * resize_ratio)
+    new_tag_size = new_tag_px_size / px_per_mm
 
-pdf_path = os.path.splitext(file_name)[0] + ".pdf"
+    image = image.convert("RGB")
+    image = image.resize((new_border_size_px, new_border_size_px), Image.NEAREST)
+    image.save(pdf_path, resolution=dpi)
+    print(f"PDF path is {pdf_path}")
+    print(f"Actual tag size is {new_tag_size} mm (enter this into the apriltag config)")
 
-dpi = px_per_mm * 25.4
 
-image = Image.open(file_name)
-image = image.convert('RGB')
-image = image.resize((new_size_px, new_size_px), Image.NEAREST)
-image.save(pdf_path, resolution=dpi)
-print("PDF path:", pdf_path)
+def main():
+    parser = argparse.ArgumentParser(description="tag")
+
+    parser.add_argument("tag", choices=list(image_formats.keys()), help="tag family")
+    parser.add_argument(
+        "codes", nargs="+", type=int, default=[], help="codes to generate"
+    )
+    parser.add_argument(
+        "-p", "--pixels_per_mm", type=float, default=10.0, help="pixels per mm"
+    )
+    parser.add_argument(
+        "-l", "--length_mm", type=float, default=200.0, help="length mm"
+    )
+
+    args = parser.parse_args()
+
+    tag_family = args.tag
+
+    if len(args.codes) == 0:
+        print("No codes provided! Nothing to do.")
+
+    for code in args.codes:
+        px_per_mm = args.pixels_per_mm
+        length_mm = args.length_mm
+
+        generate_tag_pdf(tag_family, code, px_per_mm, length_mm)
+
+
+if __name__ == "__main__":
+    main()
+
