f98de71b8c663347377f5abe27c5cc90eef12359
Woz4tetra <woz4tetra@gmail.com>
2023-08-09 08:48:39 -0400

    fix sequence script

--- src/bw_behaviors/scripts/run_sequence_script.py
+++ src/bw_behaviors/scripts/run_sequence_script.py
@@ -31,33 +31,33 @@ def main():
         disable_signals=True,
         # log_level=rospy.DEBUG
     )
-    sequences = dict(
-        startup=0,
-        charging=1,
-        driving=2,
-        hotspot=3,
-        hotspot_charging=4,
-        idle=5,
-        bot99=6,
-        fur_elise=7,
-        cs1_1pre=8,
-        cs6_1pre=9,
-        cs6_6gig=10,
-        brawl=11,
-        mii_channel=12,
-        megalovania=13,
-        ruins=14,
-        dedede=15,
-        pokemon_center=16,
-        scale3=17,
-        nokia=18
+    sequences = (
+        "startup",
+        "charging",
+        "driving",
+        "hotspot",
+        "hotspot_charging",
+        "idle",
+        "bot99",
+        "fur_elise",
+        "cs1_1pre",
+        "cs6_1pre",
+        "cs6_6gig",
+        "brawl",
+        "mii_channel",
+        "megalovania",
+        "ruins",
+        "dedede",
+        "pokemon_center",
+        "scale3",
+        "nokia",
     )
 
     parser = argparse.ArgumentParser(description="action_script")
 
     parser.add_argument("sequence",
                         default="startup",
-                        choices=list(sequences.keys()),
+                        choices=list(sequences),
                         help="Sequence name")
     parser.add_argument("-l", "--loop",
                         action="store_true",
@@ -69,16 +69,9 @@ def main():
     action.wait_for_server()
 
     goal = RunSequenceGoal()
-    try:
-        serial = int(args.sequence)
-        from_flash = False
-    except ValueError:
-        serial = sequences[args.sequence]
-        from_flash = True
     
-    goal.serial = serial
+    goal.name = args.sequence
     goal.loop = args.loop
-    goal.from_flash = from_flash
 
     action.send_goal(goal, done_cb=action_done, feedback_cb=feedback_cb)
     try:
