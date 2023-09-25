73578f7571d8345c43f0e3fc5ca0f267908bcd61
Woz4tetra <woz4tetra@gmail.com>
2023-09-24 23:56:07 -0400

    fix data excludes

--- docker/resources/install/jetson/make_symlinks.sh
+++ docker/resources/install/jetson/make_symlinks.sh
@@ -5,7 +5,7 @@ set -e
 ln -s /opt/${ORGANIZATION}/${PROJECT_NAME} ${HOME}/${PROJECT_NAME}
 
 sudo rm -r /usr/local/zed/settings
-ln -s ln /opt/${ORGANIZATION}/${PROJECT_NAME}/bw_data/data/zed/resources /usr/local/zed/settings
+ln -s /opt/${ORGANIZATION}/${PROJECT_NAME}/src/bw_data/data/zed/resources /usr/local/zed/settings
 
 sudo rm -r /usr/local/zed/resources
-ln -s ln /opt/${ORGANIZATION}/${PROJECT_NAME}/bw_data/data/zed/resources /usr/local/zed/resources
+ln -s /opt/${ORGANIZATION}/${PROJECT_NAME}/src/bw_data/data/zed/resources /usr/local/zed/resources
